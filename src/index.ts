1a8738980427cdc0cb96682bc840cfc6b80fd7cb
gregsgotlegs <greghare101@gmail.com>
2022-06-01 16:10:45 -0400

    everything2

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { formattedDate } from "../__tests__/index.tests";
+import { buildStreak, KEY, shouldIncrementOrResetStreakCount, Streak } from "./lib";
+
+export function streakCounter(storage: Storage, date: Date): Streak {
+    const streakInLocalStorage = storage.getItem(KEY);
+    if (streakInLocalStorage) {
+        try {
+            const streak = JSON.parse(streakInLocalStorage);
+            const state = shouldIncrementOrResetStreakCount(
+                date,
+                streak.lastLoginDate
+            );
+            const SHOULD_INCREMENT = state === "increment";
+            const SHOULD_RESET = state === "reset";
+
+            if (SHOULD_INCREMENT) {
+                const updatedStreak = {
+                    ...streak,
+                    currentCount: streak.currentCount + 1,
+                    lastLoginDate: formattedDate(date),
+                };
+                // store in localStorage
+                storage.setItem(KEY, JSON.stringify(updatedStreak));
+
+                return updatedStreak;
+            }
+            if (SHOULD_RESET) {
+                const resetStreak = {
+                    ...streak,
+                    currentCount: 1,
+                    lastLoginDate: formattedDate(date),
+                }
+                // store in localStorage
+                storage.setItem(KEY, JSON.stringify(resetStreak));
+
+                return resetStreak;
+            }
+            return streak;
+        } catch (error) {
+            console.error("Failed to parse streak from localStorage");
+        }
+    }
+    const streak = buildStreak(date, {});
+    
+    // store in localStorage
+    storage.setItem(KEY, JSON.stringify(streak));
+    return streak;
+}
---
+++ src/lib.ts
@@ -0,0 +1,41 @@
+import { formattedDate } from "../__tests__/index.tests";
+
+// Used when storing in localStorage
+export const KEY = "streak";
+
+export interface Streak {
+    currentCount: number;
+    startDate: string;
+    lastLoginDate: string;
+}
+
+export function shouldIncrementOrResetStreakCount(
+    currentDate: Date,
+    lastLoginDate: string
+): "increment" | "reset" | undefined {
+    // We get 11/5/2021
+    // so to get 5, we split on / and get the second item
+    const difference =
+        currentDate.getDate() - parseInt(lastLoginDate.split("/")[1]);
+    // This means they logged in the day after the currentDate
+    if (difference === 1) {
+        return "increment";
+    }
+    else if (difference !== 0){
+        return "reset";
+    }
+    // Otherwise they logged in after a day, which would
+    // break the streak
+    return undefined;
+}
+
+export function buildStreak(date: Date, partialStreak: Partial<Streak>): Streak {
+    let streak = {
+        currentCount: 1,
+        startDate: formattedDate(date),
+        lastLoginDate: formattedDate(date),
+        ...partialStreak
+    }
+
+    return streak;
+}
